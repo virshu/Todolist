ec65c8c60c1929ecf36939fab0613430f847e6a8
virshu <felix@rabinovich.org>
2020-11-28 19:51:44 -0800

    UI Tinkering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,15 +9,21 @@ import { TodoList } from './todo-list';
 })
 export class AppComponent {
   private list = new TodoList("Bob", [
-    new TodoItem("Go for run", true),
-    new TodoItem("Get flowers"),
-    new TodoItem("Collect tickets"),
+      new TodoItem("Go for run", true),
+      new TodoItem("Get flowers"),
+      new TodoItem("Collect tickets"),
     ]);
+
     get username(): string {
-    return this.list.user;
+      return this.list.user;
     }
+
     get itemCount(): number {
-    return this.list.items
-    .filter(item => !item.complete).length;
+      return this.list.items
+      .filter(item => !item.complete).length;
+    }
+
+    get items(): readonly TodoItem[] {
+      return this.list.items;
     }
 }
